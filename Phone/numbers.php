8171777386944b9b1a7a37cae2211439914f3eec
Elizabeth  Owufuni Agada <elizadave6@gmail.com>
2021-05-17 04:51:31 -0700

    Update numbers.php

--- Phone/numbers.php
+++ Phone/numbers.php
@@ -3,6 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <title>number</title>
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+	<link rel="stylesheet" href="css/font-awesome.min.css">
     <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
     <link href="css/number.css" type="text/css" rel="stylesheet">
     </head>
@@ -86,5 +88,6 @@
             </div>
             </main>
         </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
     </body>
 </html>
